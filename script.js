f3b42f2a4a49b156da3662d1a7b5128de3bea394
Darshana8916 <darshanadeore14@gmail.com>
2026-02-25 23:25:53 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+let board = Array(9).fill("");
+let currentPlayer = "x";
+let gameOver = false;
+let playerNames = {};
+
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+function startGame() {
+    const p1 = document.getElementById("player1").value || "Player 1";
+    const p2 = document.getElementById("player2").value || "Player 2";
+
+    playerNames = { X: p1, 0: p2 };
+
+
+    document.getElementById("playerInput").style.display = "none";
+    document.getElementById("gameSection").style.display = "block";
+
+    updateCurrentPlayer();
+}
+
+function makeMove(index) {
+    if (board[index]! == "" || gameOver)
+        return;
+
+    board[index] = currentPlayer;
+    document.getElementByIdClassName("cell")[index].innerText = currentPlayer;
+
+    if (checkWinner()) {
+        alert(`$ {playerName[currentPlayer]}Wins!`);
+        gameOver = true;
+        return;
+    }
+
+    if (board.every(cell => cell !== "")) {
+        alert("It's a Draw!");
+        gameOver = true;
+        return;
+    }
+    currentPlayer = currentPlayer === "X" ? "0" : "X";
+    updateCurrentPlayer();
+}
+
+function updateCurrentPlayer() {
+    document.getElementById("currentPlayer").innerText = `Current Player :${playerNames[currentPlayer]} (${currentPlayer})`;
+
+}
+function checkWinner() {
+    return winPatterns.some(Pattern) =>
+    Pattern.every(i => board[i] === currentPlayer)
+        ;
+}
+
+function resetGame() {
+    board.fill("");
+    gameOver = "false";
+    currentPlayer = "X";
+    updateCurrentPlayer();
+
+    document.querySelectorAll(".cell").forEach(cell => cell.innerText = "");
+}
